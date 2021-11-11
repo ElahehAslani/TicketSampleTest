5e0a40fbbc5e131e53751dc3759fc8baa44c8ae3
Elaheh Aslani <elahehaslani01@gmail.com>
2021-11-11 20:44:46 +0330

    add logo, favicon and change Title for projects

--- ticket-project/src/App.js
+++ ticket-project/src/App.js
@@ -7,16 +7,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          By following the links below, you can create tickets or view all your tickets.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="main">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Create Ticket
+          </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See All Tickets
+          </a>
+        </div>
+        
       </header>
     </div>
   );
