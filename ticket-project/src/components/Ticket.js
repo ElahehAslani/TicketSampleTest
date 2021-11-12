dbdcad3ea3f7e2f08287ca360c9ebf81843dc8be
Elaheh Aslani <elahehaslani01@gmail.com>
2021-11-12 16:47:20 +0330

    add form with 2 field for Title and Text

--- ticket-project/src/components/Ticket.js
+++ ticket-project/src/components/Ticket.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Ticket() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
   return (
     <div className="ticket">
-      <h2>Create Your Ticket! </h2>
+      <form className="ticket-form" onSubmit={handleSubmit(onSubmit)}>
+       <span className="ticket-span">Ticket Title:</span>
+       <input className="ticket-input" placeholder="enter your title..." {...register("ticketTitle", { required: true })} />
+       <div className="ticket-error">{errors.ticketTitle?.type === 'required' && "Please Enter Title for your Ticket."}</div>
+      
+       <span className="ticket-span">Ticket Message:</span>
+       <textarea className="ticket-message" placeholder="enter your message..." {...register("ticketMessage", { required: true })} />
+       <div className="ticket-error">{errors.ticketMessage && "Please Write your Message."}</div>
+    
+       <input className="submit" type="submit" value="Send Ticket"/>
+      </form>
     </div>
 
   );
--- ticket-project/src/pages/TicketPage.js
+++ ticket-project/src/pages/TicketPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Ticket from '../components/Ticket';
-import logo from '../logo.svg';
+import logo from '../logo.jpeg';
 import './App.css';
 
 function TicketPage() {
   return (
     <div className="ticket-page">
       <img className="image" src={logo} alt="logo" />
-      <h2>Create Your Ticket! </h2>
+      <h3>Create Your Ticket! </h3>
       <Ticket />
     </div>
 
