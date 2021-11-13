37383282e821a62c3e9dd1325978b70ebdf14566
Elaheh Aslani <elahehaslani01@gmail.com>
2021-11-13 13:18:15 +0330

    add validation fot form

--- ticket-project/src/components/Ticket.js
+++ ticket-project/src/components/Ticket.js
@@ -1,42 +1,57 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useEffect } from 'react/cjs/react.development';
 
 function Ticket() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [submitTicketButton, setSubmitTicketButton] = useState(true);
+    const { register, handleSubmit, reset, formState: { errors, isSubmitSuccessful, isDirty, isValid } } 
+    = useForm({
+        defaultValues: {
+          ticketTitle: "",
+          ticketMessage: ""
+        }
+      }
+    );
 
     const onSubmit = data => {
       console.log("data:",data);
-      if(data !== null ){
-        setSubmitTicketButton(false);
-        console.log("Submit :", submitTicketButton);
-      }
     };
+
+    console.log("is submitted:", isSubmitSuccessful);
+
+    useEffect(() => {
+      setTimeout(() => {
+        if (isSubmitSuccessful) {
+          reset({ ticketMessage: '', ticketTitle: '' });
+        }
+     }, 3000)
+     
+    }, [isSubmitSuccessful, reset]);
   
   return (
     <div className="ticket">
       <form className="ticket-form" onSubmit={handleSubmit(onSubmit)}>
-       <label htmlFor="TicketTitle" className="ticket-span">Ticket Title:</label>
-       <input 
-        className="ticket-input" 
-        placeholder="enter your title..." 
-        {...register("ticketTitle", { required: true })} 
-       />
-       <div className="ticket-error">{errors.ticketTitle?.type === 'required' && "Please Enter Title for your Ticket."}</div>
+        <label htmlFor="TicketTitle" className="ticket-span">Ticket Title:</label>
+          <input 
+          className="ticket-input" 
+          placeholder="enter your title..." 
+          {...register("ticketTitle", { required: true })} 
+        />
+        <div className="ticket-error">
+          {errors.ticketTitle?.type === 'required' && "Please Enter Title for your Ticket."}
+        </div>
       
-       <label htmlFor="TicketMessage" className="ticket-span">Ticket Message:</label>
-       <textarea 
-        className="ticket-message" placeholder="enter your message..." 
-        {...register("ticketMessage", { required: true })} 
-       />
-       <div className="ticket-error">{errors.ticketMessage && "Please Write your Message."}</div>
-       {submitTicketButton ? 
-        (<input className="submit" type="submit" value="Send Ticket"/>)
-        : 
-        (
-          <span className="after-submit" >Thank You!</span>
-        )
-       }
+        <label htmlFor="TicketMessage" className="ticket-span">Ticket Message:</label>
+        <textarea 
+          className="ticket-message" placeholder="enter your message..." 
+          {...register("ticketMessage", { required: true })} 
+        />
+        <div className="ticket-error">
+          {errors.ticketMessage && "Please Write your Message."}
+        </div>
+        <button disabled={ !isDirty } className="submit" type="submit" value="Send Ticket">
+          {isSubmitSuccessful ? "Thank Yot!" : "Send Ticket"}
+        </button>
+        {isSubmitSuccessful && <span>Your Ticket Successfully Send.</span>}
       </form>
     </div>
 
